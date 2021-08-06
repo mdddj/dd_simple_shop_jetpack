69c0b9ec791c9c7ed0f0b2ec9753b87d9a7a4209
mddd <413153189@qq.com>
2021-08-06 14:22:37 +0800

    新增首页搜索组件

--- app/src/main/java/com/example/myapplication/screens/HomeScreen.kt
+++ app/src/main/java/com/example/myapplication/screens/HomeScreen.kt
@@ -1,21 +1,18 @@
 package com.example.myapplication.screens
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.myapplication.R
 import com.example.myapplication.models.TodoViewModel
 
 
@@ -36,18 +33,28 @@ fun SearchAppbar() {
         modifier = Modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(imageVector = Icons.Filled.Home, contentDescription = "典典的小卖部")
+        Box(modifier = Modifier.width(width = 12.dp))
+        Icon(
+            painter = painterResource(id = R.drawable.svg_diandian), contentDescription = null,
+            modifier = Modifier.size(45.dp, 45.dp),
+            tint = Color.Black
+        )
         Row(modifier = Modifier.weight(1f)) {
-            Box(modifier = Modifier.padding(16.dp)) {
+            Box(modifier = Modifier.padding(horizontal = 5.dp,vertical = 5.dp)) {
                 TextField(
-                    value = "", onValueChange = {}, colors = TextFieldDefaults.textFieldColors(
+                    value = "",
+                    onValueChange = {},
+                    colors = TextFieldDefaults.textFieldColors(
                         backgroundColor = Color.White,
                         unfocusedIndicatorColor = Color.Transparent,
                         focusedIndicatorColor = Color.Transparent
                     ),
                     modifier = Modifier
                         .fillMaxWidth()
-                        .border(1.dp, Color.LightGray, CircleShape)
+                        .border(1.dp, Color.LightGray, CircleShape),
+                    label = {
+                        Text(text = "请输入搜索内容,比如辣条")
+                    }
                 )
             }
         }
