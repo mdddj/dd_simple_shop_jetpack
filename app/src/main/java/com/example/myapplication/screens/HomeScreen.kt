1f97ede60ceced6faa663b7cdb62180a097634b3
mddd <413153189@qq.com>
2021-08-06 13:46:34 +0800

    新增首页搜索组件

--- app/src/main/java/com/example/myapplication/screens/HomeScreen.kt
+++ app/src/main/java/com/example/myapplication/screens/HomeScreen.kt
@@ -4,25 +4,59 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.models.TodoViewModel
-
 
 
 @Composable
 fun HomeScreenComposable() {
-    Scaffold {
-
+    Scaffold(
+        topBar = { SearchAppbar() }
+    ) {
         Text(text = "首页")
-        
     }
 }
+
+
+// 顶部搜索栏
+@Composable
+fun SearchAppbar() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(imageVector = Icons.Filled.Home, contentDescription = "典典的小卖部")
+        Row(modifier = Modifier.weight(1f)) {
+            Box(modifier = Modifier.padding(16.dp)) {
+                TextField(
+                    value = "", onValueChange = {}, colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = Color.White,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .border(1.dp, Color.LightGray, CircleShape)
+                )
+            }
+        }
+    }
+}
+
 
 @Preview()
 @Composable
 fun TodoScreenPreview() {
+    SearchAppbar()
 }
